1de90855aec9add1f5b4f4535457170bd2a2bcc9
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2025-08-11 14:40:24 +0200

    Ajout de la gestion des requêtes OR simples

--- get_translations.py
+++ get_translations.py
@@ -45,14 +45,30 @@ def tree_to_dict_of_nodes(tree: ET._ElementTree, window, minimal_element, save_a
 
 def process_query(query="[pos='AQ.*'][pos='NC.*']"):
     """
-    Implémentation d'un parseur CQL. Pour l'instant, ne gère que des requêtes simples (pas de répétition, *+{})
+    Implémentation d'un parseur CQL. Pour l'instant, ne gère que des requêtes simples
     """
     query = query.replace("\[", "$$$").replace("\]", "^^^")
     all_tokens = re.compile(r"\[([^\]\[]+)\]")
     tokenized = [item for item in re.split(all_tokens, query) if item != ""]
     tokenized = [item.replace("$$$", "[").replace("^^^", "]") for item in tokenized]
     queries = []
-    for item in tokenized:
+    print(tokenized)
+    for idx, item in enumerate(tokenized):
+        if idx + 1 != len(tokenized) and tokenized[idx + 1] == "|":
+            print("Alternative found")
+            alternative = [tokenized[idx], tokenized[idx + 2]]
+            regexped_alternative = []
+            for i in alternative:
+                regex_query = re.compile(r"=")
+                field, expression = re.split(regex_query, i)
+                expression = fr"^{expression}$"
+                expression = re.compile(expression.replace("'", ""))
+                regexped_alternative.append((field, expression))
+            queries.append(regexped_alternative)
+            tokenized.pop(idx + 1)
+            tokenized.pop(idx + 1)
+            print(queries)
+            continue
         if "[]" in item:
             range_expression = re.compile(r"\{(\d*,\d*)\}")
             ranges = re.search(range_expression, item)
@@ -79,9 +95,21 @@ def process_query(query="[pos='AQ.*'][pos='NC.*']"):
                 expression = fr"^{expression}$"
             expression = re.compile(expression.replace("'", ""))
             queries.append((field, expression))
+    print(queries)
     out_queries = []
     # Essayons de produire toutes les requêtes possibles
-    if any(isinstance(item, range) for item in queries):
+    if any(isinstance(item, list) for item in queries):
+        print(f"Tuple found: {next(item for item in queries if isinstance(item, list))}")
+        actual_range = next(item for item in queries if isinstance(item, list))
+        copy_query_a = copy.deepcopy(queries)
+        copy_query_b = copy.deepcopy(queries)
+        alternative_a = actual_range[0]
+        alternative_b = actual_range[1]
+        copy_query_a[copy_query_a.index(actual_range)] = alternative_a
+        copy_query_b[copy_query_b.index(actual_range)] = alternative_b
+        out_queries.append(copy_query_a)
+        out_queries.append(copy_query_b)
+    elif any(isinstance(item, range) for item in queries):
         actual_range = next(item for item in queries if isinstance(item, range))
         for n in actual_range:
             copy_query = copy.deepcopy(queries)
@@ -90,8 +118,6 @@ def process_query(query="[pos='AQ.*'][pos='NC.*']"):
             out_queries.append(copy_query)
     else:
         out_queries.append(queries)
-
-
     return out_queries
 
 def check_if_token_matches(annotation, query):
@@ -105,24 +131,35 @@ def check_if_token_matches(annotation, query):
 
 def check_if_segment_matches(sentence, patterns, debug=False):
     if debug:
-        print(patterns)
+        print(f"Sentence: {sentence}")
+        print(f"Patterns: {patterns}")
     if sentence == []:
         return
+
+    # Le dernier token n'est pas reconnu, on va artificiellement en ajouter un.
+    sentence = sentence + [{}]
     match = False
-    for idx, pattern in enumerate(patterns):
+    for idx_pattern, pattern in enumerate(patterns):
         index = 0
         pattern_index_match = 0
         while not match:
             # Si on matche le nombre d'éléments de la requête, on a un match
             if pattern_index_match == len(pattern):
+                if debug:
+                    print("It's a match")
                 return True
             # Si on est à la fin de la phrase, on n'aura pas de match
             else:
                 # Dernier mot, dernière query
-                if index + 1 == len(sentence) and idx + 1 == len(patterns):
+                if index + 1 == len(sentence) and idx_pattern + 1 == len(patterns):
+                    if debug:
+                        print(sentence[index])
+                        print("End of sentence. Exiting with no result")
                     return False
                 # Dernier mot mais il reste des requêtes
-                elif index + 1 == len(sentence) and idx + 1 != len(patterns):
+                elif index + 1 == len(sentence) and idx_pattern + 1 != len(patterns):
+                    if debug:
+                        print("Break")
                     break
             current_state = sentence[index]
             try:
@@ -144,19 +181,21 @@ def check_if_segment_matches(sentence, patterns, debug=False):
                 index += 1
 
 
-def match_all_nodes(source_nodes, target_nodes, query, window, out_dir="test_results", filter="", negative_filter="", reduce=1):
+def match_all_nodes(source_nodes, target_nodes, query, window, out_dir="test_results", filter="", negative_filter="", reduce=1, debug=False):
+    results_name = f"{query}"
     if filter != "":
-        results_name = f"{query}-{filter}"
-    elif negative_filter != "":
-        results_name = f"{query}-not({negative_filter})"
-    else:
-        results_name = f"{query}"
+        results_name += f"-{filter}"
+    if negative_filter != "":
+        results_name += f"-not({negative_filter})"
     corresp_sents = {}
     query = process_query(query)
-    filter = process_query(filter)
-    negative_filter = process_query(negative_filter)
+    if filter != "":
+        filter = process_query(filter)
+    if negative_filter != "":
+        negative_filter = process_query(negative_filter)
     result = []
     for idx, (ident, node) in enumerate(source_nodes["idents"].items()):
+        corresp_segments = ', '.join(node['corresp'])
         if check_if_segment_matches(node['annotations'], query):
             # if check_if_path_matches(node['sent'], node['annotations'], processed_query, matches=0, matches_number=len(processed_query), match_first=False):
             corresponding_sents = [target_nodes["idents"][corr] for corr in node['corresp']]
@@ -166,6 +205,7 @@ def match_all_nodes(source_nodes, target_nodes, query, window, out_dir="test_res
                 continue
             if negative_filter != "" and check_if_segment_matches(all_corresp_annotations, negative_filter):
                 continue
+
             first_source_node_id = ident
             index = next(idx for idx, id in enumerate(source_nodes["ids"]) if id == first_source_node_id)
             previous_source_nodes = " ".join([source_nodes["idents"][source_nodes["ids"][rolling_idx]]["sent"] for rolling_idx in range(index - window, index)])
@@ -198,11 +238,12 @@ def match_all_nodes(source_nodes, target_nodes, query, window, out_dir="test_res
 
 
             corresp_sents[idx] = (node['corresp'], node["sent"], corresponding_sents)
-            print("---")
-            print(node['localisation'])
-            print(f"Segment: {ident}\n{source_nodes_with_context}")
-            print(corresponding_sents_with_context)
-            print(f"Corresp segments: {', '.join(node['corresp'])}")
+            if debug:
+                print(node['localisation'])
+                print(f"Segment: {ident}\n{source_nodes_with_context}")
+                print(corresponding_sents_with_context)
+                print(f"Corresp segments: {corresp_segments}")
+                print("---")
             result.append([node['localisation'],
                            ident,
                            source_nodes_with_context,
@@ -229,10 +270,10 @@ def match_all_nodes(source_nodes, target_nodes, query, window, out_dir="test_res
         table_string = table_string.replace("</b>", "}")
         result_file_tex.write(table_string)
         
-    tsv2html(f"{out_dir}/{results_name}.tsv", f"{out_dir}/{results_name}.html")
+    tsv2html(f"{out_dir}/{results_name}.tsv", f"{out_dir}/{results_name}.html", nombre_resultat=len(result))
 
 
-def tsv2html(tsv_file_name, html_file_name):
+def tsv2html(tsv_file_name, html_file_name, nombre_resultat):
     df = pd.read_csv(tsv_file_name, sep='\t', header=0)
     old_width = pd.get_option('display.max_colwidth')
     # pd.set_option('display.max_colwidth', -1)
